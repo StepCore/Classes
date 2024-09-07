c94525735f51e2a91cd13d6d681e9c446904abb5
Stepan <galantov251016@gmail.com>
2024-09-07 20:35:47 +0500

    create classmethod new product, add product, product list and property price

--- oop/classes.py
+++ oop/classes.py
@@ -1,26 +1,51 @@
 class Product:
-    name: str
-    description: str
-    price: int
-    quantity: str
-
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: int, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value: int):
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self._price = value
+
+    @classmethod
+    def new_product(cls, product_data):
+        name = product_data.get('name')
+        description = product_data.get('description')
+        price = product_data.get('price')
+        quantity = product_data.get('quantity')
+        return cls(name, description, price, quantity)
+
+    def __repr__(self):
+        return f"Product(name='{self.name}', description='{self.description}', price={self.price}, quantity={self.quantity})"
 
 
 class Category:
-    name: str
-    description: str
-    products: list
+    def __init__(self, name: str, description: str, products=None):
+        self.name = name
+        self.description = description
+        self.products = products if products else []
+        Category.category_count += 1
+        Category.product_count += len(self.products)
+
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
-        self.name = name
-        self.description = description
-        self.products = products
-        Category.category_count += 1
-        Category.product_count = len(products)
+    def add_product(self, new_product: Product):
+        self.products.append(new_product)
+        Category.product_count += 1
+
+    @property
+    def product_list(self):
+        product_str = ''
+        for product in self.products:
+            product_str += f'{product.name}, {product.price}. Остаток: {product.quantity}.\n'
+        return product_str
