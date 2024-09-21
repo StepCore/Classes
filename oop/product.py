263f0f64f489068380c64a80e0796b87b5486092
Stepan <galantov251016@gmail.com>
2024-09-22 01:34:30 +0500

    fix category.py, conftest.py and test_category.py

--- oop/product.py
+++ oop/product.py
@@ -31,4 +31,23 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт.\n"
 
     def __add__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError
         return self.__price * self.quantity + other.__price * other.quantity
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
